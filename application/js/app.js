939262dd4acb4c940e95d7a947a65653c0898b5d
ZoeJ2020 <zo8j2020@gmail.com>
2021-05-19 10:49:38 +0100

    ,

--- application/js/app.js
+++ application/js/app.js
@@ -1,8 +1,10 @@
+//Global variables
 var counter = 0;
 var tick = 0;
 
 function tableInput()
 {
+    //Takes the number user inputs and generates a random multiplication
     table = document.getElementById("table").value;
 
     times = Math.floor(Math.random() * 12) + 1;
@@ -11,18 +13,9 @@ function tableInput()
     document.getElementById("question").innerHTML = table + " x _ = " + timesResult;
 }
 
-function printTable()
-{
-    for(tick = 1; tick <=12; tick++)
-    {
-        var total = tick * table;
-
-        document.getElementById("timestables").innerHTML += tick + " x " + table + " = " + total + "<br>";
-    }
-}
-
 function checkAns()
 {
+    //Checks the user input is the same as the actual answer
     var ans = times;
     var checkAns = "";
 
@@ -30,10 +23,12 @@ function checkAns()
 
     counter++;
 
+    //Gives the user three attempts to answer
     for(var count = 0; count <3; count++)
     {
         if (ans == checkAns)
         {
+            //If answer is correct, disable buttons and inputs and show number of attempts made
             document.getElementById("feedback").innerHTML="Well done!";
             document.getElementById("feedback").style.color = "#33FF33";
             
@@ -44,16 +39,12 @@ function checkAns()
             document.getElementById("a-button").disabled=true;
             document.getElementById("b-button").disabled=true;
             
-            setTimeout(function(){
-                location.replace("app.html");
-            }, 1300);
-            {
-                break;
-            }
+            break;
         }
         
         else
         {
+            //If not, add attempt to counter and display message
             document.getElementById("feedback").innerHTML="Incorrect guess, please try again";
             document.getElementById("feedback").style.color = "#FF0000";
 
@@ -61,6 +52,7 @@ function checkAns()
         }
         if(counter==3)
         {
+            //If user runs out of attempts, disable buttons and inputs and print times table
             document.getElementById("table").disabled=true;
             document.getElementById("ans").disabled=true;
             document.getElementById("a-button").disabled=true;
@@ -71,5 +63,16 @@ function checkAns()
             printTable();
             break;
         }
+    }
+}
+
+function printTable()
+{
+    //Prints full times table for chosen number
+    for(tick = 1; tick <=12; tick++)
+    {
+        var total = tick * table;
+
+        document.getElementById("timestables").innerHTML += tick + " x " + table + " = " + total + "<br>";
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+//Global Variable
 var counter = 0;
 
 function checkUser()
 {
+    //Local Variables
     var username="adminGCC";
     var pwd="password";
     var checkUser="";
@@ -12,10 +14,13 @@ function checkUser()
 
     counter++
 
+    //Gives the user three attempts to log in
     for(var count = 0; count <3; count++)
     {
+        //Checks that user inputs match the predetermined login variables
         if (username==checkUser && pwd==checkPwd)
         {
+            //If yes, redirect to app
             document.getElementById("feedback").innerHTML="Login successful! Redirecting to application page...";
             document.getElementById("feedback").style.color="#33FF33"
             
@@ -31,6 +36,7 @@ function checkUser()
         
         else
         {
+            //If no, add attempt to counter and ask to try again
             document.getElementById("feedback").innerHTML="Incorrect, try again";
             document.getElementById("feedback").style.color="#FF0000"
 
@@ -39,6 +45,7 @@ function checkUser()
         }
         if(counter==3)
         {
+            //If attempts have ran out, disable buttons to continue and deny access
             document.getElementById("user").disabled=true;
             document.getElementById("pwd").disabled=true;
             document.getElementById("submit").disabled=true;
